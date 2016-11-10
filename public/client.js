b4906b27a0174eb1d830931a65981e38c2739848
Pan fried denzel <toubieogunleye@live.com>
2016-11-10 01:28:43 +0000

    🔥📕 whats better than 1 cat... 2 cats

--- public/client.js
+++ public/client.js
@@ -1,5 +1,11 @@
 // client-side js
 
 
-var img = document.getElementById("catty");
-var i = 0 ; img.addEventListener('click', function(e){ i += 1; document.getElementById('counter').innerText = (i) });
+var img = document.getElementsByTagName('img');
+var count = 0;
+
+function updatecounter  (e){ count += 1; document.getElementById('counter').innerText = (count) }
+
+for (i = 0; i < img.length; i ++){
+  img[i].addEventListener('click', updatecounter);
+}
