057d24f5e948555fdd86a2ba7a275640eb74f76d
Pan fried denzel <toubieogunleye@live.com>
2016-11-11 18:51:13 +0000

    😺🐘 Updated to use closures

--- public/client.js
+++ public/client.js
@@ -1,11 +1,26 @@
 // client-side js
 
 
-var img = document.getElementsByTagName('img');
+var images = [['https://cdn.hyperdev.com/aeb5754d-bad1-46c1-a5cc-af80febef884%2Funnamed.jpg','cute cat'],
+              ['https://cdn.hyperdev.com/aeb5754d-bad1-46c1-a5cc-af80febef884%2Funnamed%20(1).jpg','aww cate'],
+              ['https://cdn.hyperdev.com/aeb5754d-bad1-46c1-a5cc-af80febef884%2F0EvP5vXn.jpeg','nuke cate'],
+              ['https://cdn.hyperdev.com/aeb5754d-bad1-46c1-a5cc-af80febef884%2F3beb3e1f33a163269e0afc088113901c.jpg','booboo cate'],
+              ['https://cdn.hyperdev.com/aeb5754d-bad1-46c1-a5cc-af80febef884%2Funnamed%20(2).jpg','bae cate']];
+$('#thumb').html();
+for (i = 0; i < images.length; i++){ 
+  $('#thumb').append("<img width='150' src='"+ images[i][0]+"'/>");
+  
+  var num = i;
+}
 var count = 0;
 
-function updatecounter  (e){ count += 1; document.getElementById('counter').innerText = (count) }
-
-for (i = 0; i < img.length; i ++){
-  img[i].addEventListener('click', updatecounter);
-}
+ imgs = document.getElementsByTagName('img');
+ for(i=0; i < imgs.length; i++){
+  imgs[i].addEventListener('click', (function(i, images){
+    return function(){
+        $('#area').html("<img width='400' src='"+ images[i][0]+ "'/>");
+        count += 1; document.getElementById('counter').innerText = (count) 
+        document.getElementById('cattitle').innerText = (images[i][1]) 
+    };
+  })(i, images));
+ }
